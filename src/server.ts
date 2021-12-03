a2064b02d04b7d75fc7c17d3f15014018daf026c
André Luiz da Silva <andreluizmicro@gmail.com>
2021-12-03 15:22:33 -0300

    Adicionado configuraçoes de servidor

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
-app.listen(3333);
+app.get("/", (request, response) => {
+    return response.json({message: "I'm alive!"});
+});
+
+app.listen(3333, () => console.log('Server is running!'));
